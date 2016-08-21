f41defa3208c39a4fc69938a66e78ac1ab53d47a
Heath <palmerha@mail.uc.edu>
2016-08-21 16:52:32 -0400

    test

---
+++ script.js
@@ -0,0 +1,14 @@
+function changeImage()
+{
+document.getElementById("image").src="https://ucarecdn.com/2592be7c-a171-4fe3-83b7-3cae17d73342/R00100391_xmp.JPG";
+return false;
+}
+
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
